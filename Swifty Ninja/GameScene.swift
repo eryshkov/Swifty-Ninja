3fcb344b58e00af101053525e24c71fd6d9a145a
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-18 20:40:11 +0300

    methods created

--- Swifty Ninja/GameScene.swift
+++ Swifty Ninja/GameScene.swift
@@ -11,12 +11,36 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    
     func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        score = 0
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
         
+        addChild(gameScore)
+        
+        gameScore.position = CGPoint(x: 8, y: 8)
     }
     
     func createLives() {
-        
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            
+            addChild(spriteNode)
+            
+            livesImages.append(spriteNode)
+        }
     }
     
     func createSlices() {
