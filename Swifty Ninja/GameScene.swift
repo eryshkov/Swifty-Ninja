1d2214b7f25bf41b05deeb0a57761c4fd5935ac7
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-18 20:48:46 +0300

    createSlices() implemented

--- Swifty Ninja/GameScene.swift
+++ Swifty Ninja/GameScene.swift
@@ -21,6 +21,9 @@ class GameScene: SKScene {
     var livesImages = [SKSpriteNode]()
     var lives = 3
     
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    
     func createScore() {
         gameScore = SKLabelNode(fontNamed: "Chalkduster")
         score = 0
@@ -44,7 +47,20 @@ class GameScene: SKScene {
     }
     
     func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
         
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 2
+        
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+        
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
     }
     
     override func didMove(to view: SKView) {
